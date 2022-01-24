7f42a07e91d9a58be32af731f9409455c559450f
Matthew Martinez <mjmpoetry@gmail.com>
2022-01-24 00:35:23 -0600

    Week #1 Material

---
+++ 00_Intro_to_R/Lecture_Videos.md
@@ -0,0 +1,8 @@
+### Overview of Sllyabus
+https://utsa.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=587df61b-ce08-4b00-bcaa-ae27006bdf14
+<br/>
+
+### Quick Introduction to R and R Studio
+
+https://utsa.zoom.us/rec/share/-fKEzBsmeMQk3njAm6ugOTycLB9LHauP4PZZ6ARMnphwI9fhL6xtHCKZ2CqNQqGD.pEgeYCZvMg9GVQ6b
+
---
+++ 00_Intro_to_R/Simple_Coding.R
@@ -0,0 +1,14 @@
+#Say what I'm doing below.
+
+d3_thiswillbealongobjectname <- "Hello World"
+d = "Hello World"
+c<-3
+
+paste("Print this: ", c, c, sep="-")
+paste0("Value is: ", c, "here", d, "there")
+paste("Value is: ", c, "here", d, "there", sep=" ")
+
+#Use this if you have a question about a function
+?paste()
+??paste()
+
